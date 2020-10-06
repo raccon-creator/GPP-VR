40f78b0ceb2d14427b22a246f8d3eb8f95c09a3f
raccon-creator <2796594580@qq.com>
2020-10-06 16:12:49 +0800

    update

--- home/home.js
+++ home/home.js
@@ -1,5 +1,11 @@
 function toScene(){
-    console.log(1);
+    console.log(0);
     window.location.href="../scene/scene.html";
+    //window.navigator("http://www.baidu.com");
+};
+
+function toExample(){
+    console.log(1);
+    window.location.href="../example/example.html";
     //window.navigator("http://www.baidu.com");
 };
