d8b31f5886451fb9d7a174beffed436b45a662e3
raccon-creator <2796594580@qq.com>
2020-10-13 12:18:36 +0800

    update

--- scene/scene.js
+++ scene/scene.js
@@ -16,12 +16,12 @@ AFRAME.registerComponent('scale-on-mouseenter', {
 });
 AFRAME.registerComponent('jumpleft', {
     init: function () {
-        this.el.addEventListener('mouseenter', function () {
+        this.el.addEventListener('triggerdown', function () {
             var sceneEl = document.querySelector('a-scene');
-            if(num%3!=0){
-                num=num+1;
+            if(num>1){
+                num=num-1;
                 console.log(num);
-                sceneEl.querySelector('#panorama').setAttribute('src','../img/'+num+'.JPG');
+                sceneEl.querySelector('#panorama').setAttribute('src','#'+'p'+num);
             }
 
         });
@@ -29,12 +29,12 @@ AFRAME.registerComponent('jumpleft', {
 });
 AFRAME.registerComponent('jumpright', {
     init: function () {
-        this.el.addEventListener('mouseenter', function () {
+        this.el.addEventListener('triggerdown', function () {
             var sceneEl = document.querySelector('a-scene');
-            if(num!=1&&num!=4){
-                num=num-1;
+            if(num<10){
+                num=num+1;
                 console.log(num);
-                sceneEl.querySelector('#panorama').setAttribute('src','../img/'+num+'.JPG');
+                sceneEl.querySelector('#panorama').setAttribute('src','#'+'p'+num);
             }
         });
     }
@@ -46,7 +46,7 @@ AFRAME.registerComponent('jumpforward', {
             if(num<=3){
                 num=num+3;
                 console.log(num);
-                sceneEl.querySelector('#panorama').setAttribute('src','../img/'+num+'.JPG');
+                sceneEl.querySelector('#panorama').setAttribute('src','#'+'p'+num);
             }
         });
     }
@@ -58,8 +58,9 @@ AFRAME.registerComponent('jumpback', {
             if(num>3){
                 num=num-3;
                 console.log(num);
-                sceneEl.querySelector('#panorama').setAttribute('src','../img/'+num+'.JPG');
+                sceneEl.querySelector('#panorama').setAttribute('src','#'+'p'+num);
             }
         });
     }
 });
+
